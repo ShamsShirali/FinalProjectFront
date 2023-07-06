e3f473e7c845f4cc2e14254c724477e05f2fe2a4
unknown <shamsshsh@code.edu.az>
2023-07-06 04:39:59 +0400

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,15 @@ $(function () {
     prevArrow: `<div class="licons"><i class="fa-solid fa-arrow-left-long"></i></div>`,
     nextArrow: `<div class="ricons"><i class="fa-solid fa-arrow-right-long"></i></div>`
   });
+
+  $('.five .testimonials').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: false,
+    prevArrow: `<div class="licons"><i class="fa-solid fa-arrow-left-long"></i></div>`,
+    nextArrow: `<div class="ricons"><i class="fa-solid fa-arrow-right-long"></i></div>`
+  });
 })
 
 //geri sayim
